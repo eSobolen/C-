223cda48a7318f5b623a00e25f4af58122134897
eSobolen <43523040+eSobolen@users.noreply.github.com>
2020-12-15 15:37:12 +0300

    Add files via upload

---
+++ laba1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A test = new A();
+
+            Console.WriteLine(test.c);
+            test.c = 2;
+            Console.WriteLine(test.c);
+            Console.ReadKey();
+        }
+    }
+    class A
+    {
+        private float a = 10;
+        private float b = 10;
+
+        public float c
+        {
+            get { return a %= b; }
+            set { value = a+b; }
+        }
+    }
+}
---
+++ laba6.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		StudentComparer studentComparer = new StudentComparer();
+		Student[] students = {
+				new Student("Bill",1999,6,7,9),
+				new Student("Tom",2001,8,9,4),
+				new Student("Nick",2000,8,10,7),
+				new Student("Ted",2001,8,9,9),
+				new Student("Alex",2000,7,6,5)
+			};
+
+		int sortedHead = 0;
+		for (int i = 0; i < students.Length; i++)
+		{
+			for (int j = sortedHead; j < students.Length; j++)
+			{
+				if (students[i].Year > students[j].Year)
+				{
+					(students[i], students[j]) = (students[j], students[i]);
+				}
+			}
+			sortedHead++;
+		}
+
+		Console.WriteLine("Sorted by Year");
+
+		foreach (Student student in students)
+		{
+			student.print();
+		}
+
+		Array.Sort(students, studentComparer);
+
+		Console.WriteLine("Sorted by Name");
+
+		foreach (Student student in students)
+		{
+			student.print();
+		}
+	}
+}
+
+class Human : IComparable<Human>
+{
+	private string name;
+	private int year;
+	private string state;
+
+	public string Name
+	{
+		set { name = value; }
+		get { return name; }
+	}
+
+	public int Year
+	{
+		set { year = value; }
+		get { return year; }
+	}
+
+	public Human() { }
+
+	public Human(string name, int year, string state)
+	{
+		this.name = name;
+		this.year = year;
+		this.state = state;
+	}
+
+	public void print()
+	{
+		Console.WriteLine(name + ";" + year + ";" + state + ";");
+	}
+
+	public virtual int info()
+	{
+		return DateTime.Today.Year - year;
+	}
+
+	public int CompareTo(Human p)
+	{
+		return this.Name.CompareTo(p.name);
+	}
+}
+
+class Student : Human
+{
+	private int gradeMath;
+	private int gradePhisics;
+	private int gradeHistory;
+
+	public Student() : base() { }
+
+	public Student(string name, int year, int gradeMath, int gradePhisics, int gradeHistory) : base(name, year, "Student")
+	{
+		this.gradeMath = gradeMath;
+		this.gradePhisics = gradePhisics;
+		this.gradeHistory = gradeHistory;
+	}
+
+	public double getAvarageGrade()
+	{
+		return (gradeHistory + gradeMath + gradePhisics) / 3.0;
+	}
+
+	public int Info()
+	{
+		int[] grades = new int[3];
+		grades[0] = gradePhisics;
+		grades[1] = gradeMath;
+		grades[2] = gradeHistory;
+		return grades.max();
+	}
+}
+
+class StudentComparer : IComparer
+{
+	public int Compare(object x, object y)
+	{
+		return string.Compare(((Student)x).Name, ((Student)y).Name);
+	}
+}
